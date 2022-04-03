a7d6e3d1bda1d5c8d42fcf966c6aa56c76a63347
furkan <furkanyilmaz-@hotmail.com>
2022-04-03 03:09:35 +0300

    Login Page Validation added closes #7

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,6 +2,44 @@ import React, { useState, useEffect } from "react";
 import brand from "../images/brand.jpg";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(email, password));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(email, password);
+    }
+  }, [formErrors]);
+
+  const validate = (email, password) => {
+    const errors = {};
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
+
+    if (!email) {
+      errors.email = "E mail is required!";
+    } else if (!regex.test(email)) {
+      errors.email = "This is not valid e mail format!";
+    }
+    console.log(password);
+    if (!password) {
+      errors.password = "Password is required!";
+    } else if (password.length < 4) {
+      errors.password = "Password must be more than 4 characters!";
+      console.log("execute");
+    } else if (password.length > 12) {
+      errors.password = "Password must be less than 12 characters!";
+    }
+    return errors;
+  };
   return (
     <figure className="w-full grid grid-rows justify-center">
       <div className="w-48 ml-32">
@@ -16,19 +54,25 @@ export default function LoginPage() {
               Login to your account
             </h1>
           </div>
-          <form>
+          <form onSubmit={handleSubmit}>
+            <p className="text-red-500">{formErrors.email}</p>
             <label className="text-left">E mail:</label>
             <input
               name="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="e mail"
               className={
                 "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
               }
             />
+            <p className="text-red-500">{formErrors.password}</p>
             <label>Password:</label>
             <input
               name="password"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               className={
                 "w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"
