dc96fed4b03a696a29e79ca8e60600e2f9f2894f
furkan <furkanyilmaz-@hotmail.com>
2022-06-08 01:30:18 +0300

    favorite Cards added closes #23

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -14,9 +14,7 @@ const Tabs = ({ color }) => {
               <a
                 className={
                   "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                  (openTab === 1
-                    ? "text-white bg-" + color + "-600"
-                    : "text-" + color + "-600 bg-white")
+                  (openTab === 1 ? "text-blue-400" : "text bg-blue-400")
                 }
                 onClick={(e) => {
                   e.preventDefault();
@@ -26,16 +24,14 @@ const Tabs = ({ color }) => {
                 href="#link1"
                 role="tablist"
               >
-                Profile
+                Comments
               </a>
             </li>
             <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
               <a
                 className={
                   "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                  (openTab === 2
-                    ? "text-white bg-" + color + "-600"
-                    : "text-" + color + "-600 bg-white")
+                  (openTab === 2 ? "text-blue-400 " : "text bg-blue-400")
                 }
                 onClick={(e) => {
                   e.preventDefault();
@@ -45,26 +41,7 @@ const Tabs = ({ color }) => {
                 href="#link2"
                 role="tablist"
               >
-                Settings
-              </a>
-            </li>
-            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-              <a
-                className={
-                  "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                  (openTab === 3
-                    ? "text-white bg-" + color + "-600"
-                    : "text-" + color + "-600 bg-white")
-                }
-                onClick={(e) => {
-                  e.preventDefault();
-                  setOpenTab(3);
-                }}
-                data-toggle="tab"
-                href="#link3"
-                role="tablist"
-              >
-                Options
+                Favorites
               </a>
             </li>
           </ul>
@@ -73,34 +50,55 @@ const Tabs = ({ color }) => {
               <div className="tab-content tab-space">
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
                   <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate B2C users
-                    after installed base benefits.
-                    <br />
-                    <br /> Dramatically visualize customer directed convergence
-                    without revolutionary ROI.
+                    Furkan Yilmaz Kız Kulesine yorum yaptı. <br />
+                    <br /> Furkan Yilmaz Galata Köprüsüne yorum yaptı.
                   </p>
                 </div>
                 <div className={openTab === 2 ? "block" : "hidden"} id="link2">
-                  <p>
-                    Completely synergize resource taxing relationships via
-                    premier niche markets. Professionally cultivate one-to-one
-                    customer service with robust ideas.
-                    <br />
-                    <br />
-                    Dynamically innovate resource-leveling customer service for
-                    state of the art customer service.
-                  </p>
+                  <div className="flex flex-auto">
+                    <div className="pr-48">
+                      <img
+                        className="w-72 ml-12 rounded-lg"
+                        src="https://media.istockphoto.com/photos/notre-dame-de-paris-cathedral-france-picture-id465616584?k=20&m=465616584&s=612x612&w=0&h=36toSt_gCCrq_zCSEnV5zp78qew9faE0LskP_Of7F_U="
+                      ></img>
+                      <div className="pt-4 px-12">
+                        <p className="text-[#011847] font-bold text*xl mt-2">
+                          Notre Dame Katedrali
+                        </p>
+                        <div className="text-[#45413E]">
+                          Notre Dame Katedrali Paris, Fransa'da bulunan dünyaca
+                          ünlü bir katedraldir. Meryem Ana'ya ithafen
+                          isimlendirilmiştir. Gotik yapı Île de la Cité'nin doğu
+                          kısmında, Paris'in diğer tüm önemli yapıları gibi
+                          Seine Nehri'nin kıyısında bulunur. Girişi batıya
+                          bakar.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bg-cover">
+                      <img
+                        className="w-72 ml-12 rounded-lg"
+                        src="https://i1.wp.com/blog.eyobus.com.tr/wp-content/uploads/2017/05/Eyfel-Kulesi-Paris.jpg?resize=640%2C400&ssl=1"
+                      ></img>
+                      <div className="pt-4 px-12">
+                        <p className="text-[#011847] font-bold text*xl mt-2">
+                          Eyfel kulesi
+                        </p>
+                        <div className="text-[#45413E]">
+                          Eyfel Kulesi, Paris'teki demir kule. Kule, aynı
+                          zamanda tüm dünyada Fransa'nın sembolü halini
+                          almıştır. İsmini, inşa ettiren Fransız mimar Gustave
+                          Eiffel'den alır. En büyük turizm cazibelerinden biri
+                          olan Eyfel Kulesi, yılda 6 milyon turist çeker. 2002
+                          yılında toplam ziyaretçi sayısı 200 milyona
+                          ulaşmıştır.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className={openTab === 3 ? "block" : "hidden"} id="link3">
-                  <p>
-                    Efficiently unleash cross-media information without
-                    cross-media value. Quickly maximize timely deliverables for
-                    real-time schemas.
-                    <br />
-                    <br /> Dramatically maintain clicks-and-mortar solutions
-                    without functional solutions.
-                  </p>
+                  <p></p>
                 </div>
               </div>
             </div>
@@ -114,7 +112,7 @@ const Tabs = ({ color }) => {
 export default function TabsRender() {
   return (
     <>
-      return <Tabs color="pink" />;
+      <Tabs />
     </>
   );
 }
